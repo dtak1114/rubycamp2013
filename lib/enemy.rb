3cdc53fb1ad26c8d648c0a9cc217dce96b94d5ec
Ssaki Hikaru <h.sasaki622193@gmail.com>
2013-02-27 14:26:48 +0900

    敵の動き方をランダムで変化させない

--- lib/enemy.rb
+++ lib/enemy.rb
@@ -6,14 +6,12 @@ class Enemy < Sprite
     super
     @seafront = Configure::SEAFRONT_THRESHOLD #海岸線を暫定的に決定　
     @count_update = 0 #updateを行った回数を数える   
-    @move_v = 0
+    @move_v = rand(10) - 5
   end
 
   def update()
     unless @stopped #移動の処理
-      if @count_update % 60 == 0 #once in a second
-        @move_v = rand(10) - 5  #移動速度の変化
-      end
+      
       #移動
       @move_v *= -1 if (self.x <= 0 || self.x > Configure::WINDOW_WIDTH - Configure::ENEMY_IMG_WIDTH)
 
