3cb16795f5e30414e17d53d7e5bccf08323d4046
Daisuke Takahashi <d.takahashi1811@gmail.com>
2013-03-01 08:14:37 +0900

    added sound effect

--- lib/director.rb
+++ lib/director.rb
@@ -36,7 +36,8 @@ class Director
     @pants_img.setColorKey([0, 0, 0])
 
     #BGM
-    bgm = Sound.new("sound/bgm.mid") 
+    @bgm = Sound.new("sound/bgm.mid") 
+    #SE
 
     @player = Player.new(Configure::PLAYER_INIT_X, Configure::PLAYER_INIT_Y, @player_img)
     #Player's position at the beggining of the game
@@ -91,7 +92,7 @@ class Director
 
   def play
     # refresh per frame 
-    bgm.play
+    @bgm.play
 
     # background initialize
     @map.scroll
