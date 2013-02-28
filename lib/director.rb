c32ae40f2d056f0f555741105b1fb64a7914ee73
makotchan <Utiwa_6@yahoo.co.jp>
2013-02-28 14:46:33 +0900

    marge

--- lib/director.rb
+++ lib/director.rb
@@ -6,6 +6,7 @@ class Director
   def initialize
     @map = Map.new
     @score = Score.new
+
     @score.map = self
 
     # 海岸の画像の設定
@@ -17,11 +18,13 @@ class Director
 
     @damege_img = Image.load("./images/lake2.jpg")
     @death_img = Image.load("./images/lake3.jpg")
+
+
     @player_img = Image.load("./images/player.png")
-    
     @player_img.setColorKey([0, 0, 0])
-    
-    #画像を読み込んでおく
+
+#画像を読み込んでおく
+
     @enemy_img = Image.load("./images/enemy.png")
     @enemy_img.setColorKey([0, 0, 0])
     @bullet_img = Image.load("./images/bullet.png")
@@ -114,7 +117,7 @@ class Director
     @player.draw
 
     Bullet.fire(@bullets,@bullet_img,@player.x,@player.y,@player.angle)
-    Pants.fire(@pants,@pants_img,@player.x,@player.y,@player.angle)
+    Pants.fire(@pants, @pants_img, @player.x, @player.y, @player.angle, @score.point)
 
     #explode shot
     @explode.update
